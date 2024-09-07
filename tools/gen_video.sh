701fc2dcb8fa01990930fd433f1fff682246ba80
Sebastien Tourneux <sebastientourneux1@gmail.com>
2024-09-07 12:24:29 +0200

    FIX process video encoding

--- tools/gen_video.sh
+++ tools/gen_video.sh
@@ -10,15 +10,15 @@ TMP_DIR="output_tmp"
 rm -rf "$TMP_DIR"
 mkdir "$TMP_DIR"
 
-python ./tools/rename.py ./output "$TMP_DIR"
+python ./tools/rename.py ./output_second_book "$TMP_DIR"
 
 FFMPEG_COMMAND="ffmpeg -y -framerate 8  -i \"$TMP_DIR/image_%04d.png\" -vf \"scale=800:-1,pad=800:800:-1:-1, tpad=stop_mode=clone:stop_duration=2\" -sws_flags neighbor"
 
 echo "FFMPEG_COMMAND $FFMPEG_COMMAND"
 
-eval $FFMPEG_COMMAND process_2.mp4
+eval $FFMPEG_COMMAND process_3.mp4
 
 rm -rf "$TMP_DIR"
 
-FFMPEG_COMMAND_CONCAT="ffmpeg -y -f concat -safe 0 -i video_files.txt -c copy "
+FFMPEG_COMMAND_CONCAT="ffmpeg -y -f concat -safe 0 -i video_files.txt -c:v libx264 -crf 10 -vf format=yuv420p"
 eval $FFMPEG_COMMAND_CONCAT process.mp4
